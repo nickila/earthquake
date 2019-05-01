ef30f8aedc5c45d1e879f2c21e39a6beec807340
Brian Nickila <brian.nickila@gmail.com>
2019-05-01 13:33:00 -0500

    Started creating overlays on click

--- script/app.js
+++ script/app.js
@@ -7,10 +7,19 @@ $(document).ready(function () {
         if (x < 345 && x > 315 && y < 357 && y > 335) {
             $("#red-overlay").addClass("show")
         }
-
+        if (x < 1015 && x > 989 && y < 228 && y > 205) {
+            $("#orange-overlay-one").addClass("show")
+        }
+        if (x < 700 && x > 675 && y < 300 && y > 280) {
+            $("#orange-overlay-two").addClass("show")
+        }
+        if (x < 898 && x > 873 && y < 382 && y > 375) {
+            $("#orange-overlay-three").addClass("show")
+        }
     })
     $(".close-overlay").on("click", function () {
         $("#red-overlay").removeClass("show")
+        $(this).parent().removeClass("show")
     })
 
 }) 
